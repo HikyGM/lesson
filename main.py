1cbaaaa3e018cbdca9fba0bb8fb6bbfd863ab4fc
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-09 11:32:12 +0500

    1.7 True/False

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+count = int(input())
+bool_continent = bool(True)
+for i in range(count):
+    n = input()
+    if n == 'С кем война?':
+        if bool_continent:
+            print('Евразия')
+        elif not bool_continent:
+            print('Остазия')
+    elif n == 'С кем мир?':
+        if not bool_continent:
+            print('Евразия')
+        elif bool_continent:
+            print('Остазия')
+    elif n == 'Меняем':
+        if bool_continent:
+            bool_continent = False
+        elif not bool_continent:
+            bool_continent = True
