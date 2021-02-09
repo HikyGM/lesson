6c4155fe67e020218a5989727524fbde74fd83bf
HikyGM <29032000Tk>
2021-02-09 22:41:29 +0300

    1.8 Рисуем прямоугольник

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-max_count = int(input())
-count_error = 0
-countdown_end = 0
-count = 1
-while max_count > count_error:
-    countdown = str(input())
-    if count == 1 and countdown == 'раз':
-        count += 1
-        countdown_end += 1
-    elif count == 2 and countdown == 'два':
-        count += 1
-        countdown_end += 1
-    elif count == 3 and countdown == 'три':
-        count += 1
-        countdown_end += 1
-    elif count == 4 and countdown == 'четыре':
-        count = 1
-        countdown_end += 1
-    else:
-        count_error += 1
-        print('Правильных отсчётов было ', countdown_end, sep='', end='')
-        print(', но теперь вы ошиблись.')
-        count = 1
-        countdown_end = 0
-    if max_count <= count_error:
-        print('На сегодня хватит.')
-        break
+n = int(input())
+m = int(input())
+symb = str(input())
+print(symb * m)
+for i in range(n - 2):
+    print(symb, ' ' * (m - 2), symb, sep='')
+print(symb * m)
