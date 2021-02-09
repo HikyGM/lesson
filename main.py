1896cc1b708ddf4ef5f7a3847277ec12736ac960
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-09 12:02:50 +0500

    1.7 Найди кота (break)

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-count = int(input())
-bool_continent = bool(True)
-for i in range(count):
-    n = input()
-    if n == 'С кем война?':
-        if bool_continent:
-            print('Евразия')
-        elif not bool_continent:
-            print('Остазия')
-    elif n == 'С кем мир?':
-        if not bool_continent:
-            print('Евразия')
-        elif bool_continent:
-            print('Остазия')
-    elif n == 'Меняем':
-        if bool_continent:
-            bool_continent = False
-        elif not bool_continent:
-            bool_continent = True
+n = int(input())
+bool_fra = False
+for i in range(n):
+    fr = str(input())
+    fra = fr.lower()
+    if 'кот' in fra:
+        bool_fra = True
+    if bool_fra:
+        print('МЯУ')
+        break
+if not bool_fra:
+    print('НЕТ')
