994007a973fb075730d31421f3657c5ef7079a6b
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-09 12:43:37 +0500

    1.7 Найди кота — 3

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 bool_fra = False
 count = 0
+count2 = 0
 fr = ''
 while fr != 'СТОП':
     fr = str(input())
     fra = fr.lower()
-    count += 1
+    if not bool_fra:
+        count += 1
     if 'кот' in fra:
         bool_fra = True
-        print(count)
-        break
-if not bool_fra:
-    print('-1')
+        count2 += 1
+        continue
+if bool_fra:
+    print(count2, count)
+elif not bool_fra:
+    print('0 -1')
