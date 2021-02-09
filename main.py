71643b8c390c656f10de8fb910644e30af94c703
HikyGM <29032000Tk>
2021-02-09 22:16:24 +0300

    1.7 Школа танцев

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-n = int(input())
-m = int(input())
-x = False
-while m != 0:
-    if not x:
-        if n < m:
-            a = m
-            x = True
-    if x:
-        if n > m:
-            b = m
-            break
-    n = m
-    m = int(input())
-print(a, b, b - a)
+max_count = int(input())
+count_error = 0
+countdown_end = 0
+count = 1
+while max_count > count_error:
+    countdown = str(input())
+    if count == 1 and countdown == 'раз':
+        count += 1
+        countdown_end += 1
+    elif count == 2 and countdown == 'два':
+        count += 1
+        countdown_end += 1
+    elif count == 3 and countdown == 'три':
+        count += 1
+        countdown_end += 1
+    elif count == 4 and countdown == 'четыре':
+        count = 1
+        countdown_end += 1
+    else:
+        count_error += 1
+        print('Правильных отсчётов было ', countdown_end, sep='', end='')
+        print(', но теперь вы ошиблись.')
+        count = 1
+        countdown_end = 0
+    if max_count <= count_error:
+        print('На сегодня хватит.')
+        break
