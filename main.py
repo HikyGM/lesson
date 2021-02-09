995cdfbbe1c6de20b9c4d235a274a9bb5b6420c5
HikyGM <29032000Tk>
2021-02-10 00:27:04 +0300

    1.8 Логистический максимин ???

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 n = int(input())
+min_size = 0
+count = 0
 for i in range(1, n + 1):
-    for j in range(i, 0, -1):
-        print('Осталось секунд:', (j - 1))
-    print('Пуск', i)
+    count_way = int(input())
+    max_height_tunnel = 500
+    for j in range(1, count_way + 1):
+        tunnel_height = int(input())
+        if max_height_tunnel > tunnel_height:
+            max_height_tunnel = tunnel_height
+            continue
+    if min_size < max_height_tunnel:
+        min_size = max_height_tunnel
+        count = i
+print(count, min_size)
