5f07b06ab99ae742421d4290bad484de23a6f9f0
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-15 09:07:06 +0500

    1.8 Начинающий фермер

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = int(input())
-for i in range(2, n):
-    prime = True
-    for j in range(2, i):
-        if i % j == 0:
-            prime = False
-            break
-    if prime:
-        print(i)
+subsid = int(input())
+count_skot = int(input())
+for i in range(1, min(count_skot, subsid // 20) + 1):
+    for j in range(0, min(count_skot, subsid // 10) + 1):
+        for k in range(0, min(count_skot, subsid // 5) + 1):
+            if i * 20 + j * 10 + k * 5 == subsid:
+                if i + j + k == count_skot:
+                    print(i, j, k)
