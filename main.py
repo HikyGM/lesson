e659fff7a19da641a4ebe166f7887729ae4c47fb
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-15 10:23:59 +0500

    2.1 Новые блюда

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-men = int(input())
-men_works = []
-for _ in range(men):
-    men_works.append(input())
-result = 0
-for example in set(men_works):
-    cout = 0
-    for name in men_works:
-        if example == name:
-            cout += 1
-    if cout > 1:
-        result += cout
-print(result)
+name_food = set()
+count_food = int(input())
+for i in range(count_food):
+    name_food.add(str(input()))
+count_days = int(input())
+for i in range(count_days):
+    for j in range(int(input())):
+        food = str(input())
+        if food in name_food:
+            name_food.remove(food)
+for i in name_food:
+    print(i)
