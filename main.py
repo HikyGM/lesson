1e7e4be671f1c073e01565faf2aefd3f707741a7
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-15 08:52:20 +0500

    1.8 Простые числа на миллион долларов

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 n = int(input())
-min_size = 0
-count = 0
-for i in range(1, n + 1):
-    count_way = int(input())
-    max_height_tunnel = 500
-    for j in range(1, count_way + 1):
-        tunnel_height = int(input())
-        if max_height_tunnel > tunnel_height:
-            max_height_tunnel = tunnel_height
-            continue
-    if min_size < max_height_tunnel:
-        min_size = max_height_tunnel
-        count = i
-print(count, min_size)
+for i in range(2, n):
+    prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            prime = False
+            break
+    if prime:
+        print(i)
