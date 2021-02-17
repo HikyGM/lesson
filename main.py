5a7b5bab276a2e6378c731eb338178181360b0e6
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-17 15:28:21 +0500

    2.2 Игра в города

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-name_food = set()
-count_food = int(input())
-for i in range(count_food):
-    name_food.add(str(input()))
-count_days = int(input())
-for i in range(count_days):
-    for j in range(int(input())):
-        food = str(input())
-        if food in name_food:
-            name_food.remove(food)
-for i in name_food:
-    print(i)
+a = str(input())
+b = str(input())
+while a[-1] == b[0]:
+    a = b
+    b = str(input())
+else:
+    print(b)
