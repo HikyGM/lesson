87b0db2a841e0d75a55ae7e6fdece4e9095f811c
HikyGM <29032000Tk>
2021-02-20 22:16:19 +0300

    2.3 Шах и мат, программисты

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-text1 = str(input())
-text2 = str(input())
-count_bull = 0
-count_cow = 0
-for i in range(len(text1)):
-    if text1[i] == text2[i]:
-        count_bull += 1
-    elif text2[i] in text1:
-        count_cow += 1
-print(count_bull, count_cow)
+number_of_cells = int(input())
+letter = ' ABCDEFGHI'
+for i in range(number_of_cells, 0, -1):
+    for j in range(1, number_of_cells + 1):
+        print(letter[j] + str(i), end=' ')
+    print('\n')
