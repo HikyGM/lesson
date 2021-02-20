4478fba8bcb367ed631d28b0d226ef20d2f4ac7c
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-20 14:22:20 +0500

    1.6 Шварценеггер против Годзиллы

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-print(ord('А'))
-print(ord('Я'))
-print(ord('а'))
-print(ord('я'))
-
-count = int(input())
-text = str(input())
-for i in range(len(text)):
-    if 1103 >= ord(text[i]) >= 1040:
-        symbol = ord(text[i]) + count
-        if 1103 >= symbol >= 1040:
-            print(chr(symbol), end='')
-
-    else:
-        print(chr(ord(text[i])), end='')
+count_drob = int(input())
+a = 0
+b = 0
+for i in range(count_drob):
+    n = int(input())
+    m = int(input())
+    if count_drob == 1:
+        a = n
+        b = m
+    elif a == 0 and b == 0:
+        a = n
+        b = m
+        continue
+    a = (a * m) + (n * b)
+    b = (m * b)
+    ch = a
+    zn = b
+    q = a
+    while zn % ch != 0:
+        r = zn % ch
+        zn, q, ch = ch, zn // ch, r
+    b = b // ch
+    if count_drob == 1:
+        a = 1
+        continue
+    a = a // ch
+print(a, '/', b, sep='')
