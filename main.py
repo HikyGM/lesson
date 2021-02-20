a7c0ed95b113503091263c03a096aa61cbdd6c4e
HikyGM <29032000Tk>
2021-02-21 01:27:30 +0300

    2.5 Произведение

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-result = str(input())
-number_of_positions = int(result[:3])
-the_amount = int(result[4:])
-error_list = []
-summa = 0
-for i in range(number_of_positions):
-    string = str(input())
-    price_product = int(string[:6])
-    count_products = int(string[8:11])
-    result_price_products = int(string[13:])
-    summa = summa + (price_product * count_products)
-    if price_product * count_products != result_price_products:
-        error_list.append(i)
-n = the_amount - summa
-if abs(n) == 0:
-    print(n)
+count_number = int(input())
+list_number = []
+log = False
+for i in range(count_number):
+    list_number.append(int(input()))
+number = int(input())
+for i in range(len(list_number) - 1):
+    for j in range(i + 1, len(list_number)):
+        if list_number[i] * list_number[j] == number:
+            log = True
+            break
+if log:
+    print('ДА')
 else:
-    print(n)
-    for i in range(len(error_list)):
-        print(error_list[i] + 1, end=' ')
+    print('НЕТ')
