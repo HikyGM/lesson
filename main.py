d898949e2098fd2f8137412e5e85bfe54b291882
HikyGM <29032000Tk>
2021-02-21 01:59:38 +0300

    2.5 Сортировка по алфавиту

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 count_number = int(input())
 list = []
-list_number = []
 for i in range(count_number):
     name = str(input())
     list.append(name)
-    if name[-1] == '4' or name[-1] == '5':
-        list_number.append(name)
+for i in range(count_number - 1):
+    for j in range(count_number - 1 - i):
+        if list[j] > list[j + 1]:
+            list[j], list[j + 1] = list[j + 1], list[j]
 for i in range(count_number):
     print(list[i])
-print('')
-for i in range(len(list_number)):
-    print(list_number[i])
