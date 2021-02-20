b61f099ec6bd4c20d8b254a92f0a3e0d0ff69f9a
HikyGM <29032000Tk>
2021-02-21 00:01:32 +0300

    2.4 Белый список

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 number_of_string = int(input())
-list_number = []
+white_list = []
+allowed_request = []
 for i in range(number_of_string):
-    number = int(input())
-    list_number.append(number)
-for i in range(number_of_string - 1):
-    print(list_number[i] + list_number[i + 1])
+    string = str(input())
+    white_list.append(string)
+number_of_search = int(input())
+for i in range(number_of_search):
+    string = str(input())
+    if string in white_list:
+        allowed_request.append(string)
+for i in range(len(allowed_request)):
+    print(allowed_request[i])
