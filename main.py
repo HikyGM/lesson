895e881378fe045178bb6fc8e6602dbe0d0acb3f
Алексей Горнеев <gorneev1998@mail.ru>
2021-02-20 11:43:25 +0500

    2.2 Цезарьь его знает (не готово)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-a = str(input())
-b = str(input())
-while a[-1] == b[0]:
-    a = b
-    b = str(input())
-else:
-    print(b)
+print(ord('А'))
+print(ord('Я'))
+print(ord('а'))
+print(ord('я'))
+
+count = int(input())
+text = str(input())
+for i in range(len(text)):
+    if 1103 >= ord(text[i]) >= 1040:
+        symbol = ord(text[i]) + count
+        if 1103 >= symbol >= 1040:
+            print(chr(symbol), end='')
+
+    else:
+        print(chr(ord(text[i])), end='')
