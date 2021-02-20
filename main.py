3fa2109e0a33df83b7b9be62dbb63e4541deb582
HikyGM <29032000Tk>
2021-02-21 02:17:30 +0300

    2.5 Сортировка в обратном порядке

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 count_number = int(input())
 list = []
 for i in range(count_number):
-    name = str(input())
+    name = int(input())
     list.append(name)
 for i in range(count_number - 1):
     for j in range(count_number - 1 - i):
-        if int(len(list[j])) > int(len(list[j + 1])):
+        if list[j] < list[j + 1]:
             list[j], list[j + 1] = list[j + 1], list[j]
-        elif int(len(list[j])) == int(len(list[j + 1])):
-            if list[j] > list[j + 1]:
+        elif list[j] == list[j + 1]:
+            if list[j] < list[j + 1]:
                 list[j], list[j + 1] = list[j + 1], list[j]
 for i in range(count_number):
     print(list[i])
