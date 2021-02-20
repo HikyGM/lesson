1454df5d61ebedf9644115c36e1c8ec2a6d135d5
HikyGM <29032000Tk>
2021-02-20 22:02:02 +0300

    2.3 Быки и коровы

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-text = str(input())
-symbol = int(input())
-text_complete = ' ' + text
-if 0 < symbol < len(text_complete):
-    print(text_complete[symbol])
-else:
-    print('ОШИБКА')
+text1 = str(input())
+text2 = str(input())
+count_bull = 0
+count_cow = 0
+for i in range(len(text1)):
+    if text1[i] == text2[i]:
+        count_bull += 1
+    elif text2[i] in text1:
+        count_cow += 1
+print(count_bull, count_cow)
