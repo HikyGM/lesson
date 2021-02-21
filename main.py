70719830d43978c7f2d44c7e09b816c3e8d608d6
HikyGM <29032000Tk>
2021-02-21 22:43:51 +0300

    2.6 /etc/passwd

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-count_string = int(input())
-list_spisok = []
-for i in range(count_string):
-    spisok = str(input())
-    if 'лук' in spisok:
-        continue
-    list_spisok.append(spisok)
-print(', '.join(list_spisok))
+spisok = []
+n = list(input().split(':'))
+while n != ['']:
+    spisok.append(n)
+    n = list(input().split(':'))
+c = list(input().split(';'))
+for i in range(len(spisok)):
+    if c.count(spisok[i][1]) != 0:
+        print('To:', spisok[i][0])
+        m = spisok[i][4].split(',')
+        print(m[0] + ',' + ' ваш пароль слишком простой, смените его.')
