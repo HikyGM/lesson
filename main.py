9b9491f2824a49df9256953d05bf29f27c5ed897
HikyGM <29032000Tk>
2021-02-21 22:33:59 +0300

    2.6 Только без лука!

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-string = str(input()).split()
-print('\n'.join(int(string[i]) * '*' for i in range(len(string))))
+count_string = int(input())
+list_spisok = []
+for i in range(count_string):
+    spisok = str(input())
+    if 'лук' in spisok:
+        continue
+    list_spisok.append(spisok)
+print(', '.join(list_spisok))
