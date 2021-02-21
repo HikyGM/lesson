8003831f242a67c64373b26b1afb8ce6a2bf4eaa
HikyGM <29032000Tk>
2021-02-21 22:24:08 +0300

    2.6 Горизонтальная диаграмма

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-count_number = int(input())
-list = []
-for i in range(count_number):
-    name = int(input())
-    list.append(name)
-for i in range(count_number - 1):
-    for j in range(count_number - 1 - i):
-        if list[j] < list[j + 1]:
-            list[j], list[j + 1] = list[j + 1], list[j]
-        elif list[j] == list[j + 1]:
-            if list[j] < list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
-for i in range(count_number):
-    print(list[i])
+string = str(input()).split()
+print('\n'.join(int(string[i]) * '*' for i in range(len(string))))
