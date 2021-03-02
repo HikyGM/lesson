c40e4b994bd1dd19c82c87a090a3f8adece8f791
Алексей Горнеев <gorneev1998@mail.ru>
2021-03-02 12:13:51 +0500

    2.7 Пристраиваемся в очередь

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-m = input().split()
-a = 0
-for i in range(len(m)):
-    a = a + len(m[i])
-print(a)
+sp = []
+one = ['встала', 'встал', 'в', 'очередь.']
+two = ['Привет,', 'будет', 'за', 'тобой.']
+three = ['хватит', 'тут', 'стоять,', 'пошли', 'отсюда.']
+for i in range(int(input())):
+    b = input()
+    if 'встал' in b or 'встала' in b:
+        x = ''
+        for j in b.split():
+            if j not in one:
+                x = x + j + ' '
+        sp.append(x.strip())
+
+    elif 'Привет,' in b:
+        c = b.split('! ')[0][8:]
+        index = sp.index(c)
+        x = ''
+        for j in b.split():
+            if j not in two and '!' not in j:
+                if j == c:
+                    x += j + ' '
+                if j not in c:
+                    x += j + ' '
+        sp.insert(index + 1, x.strip())
+
+    elif 'хватит' in b:
+        s = b.split(', ')[0]
+        sp.pop(sp.index(s))
+
+for i in sp:
+    print(i)
