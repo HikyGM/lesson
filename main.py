64b3b136b33684c22532f3acf381e5cfdc5127df
Алексей Горнеев <gorneev1998@mail.ru>
2021-03-02 10:26:24 +0500

    2.6 Джек-Победитель-Великанов

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-spisok = []
-n = list(input().split(':'))
-while n != ['']:
-    spisok.append(n)
-    n = list(input().split(':'))
-c = list(input().split(';'))
-for i in range(len(spisok)):
-    if c.count(spisok[i][1]) != 0:
-        print('To:', spisok[i][0])
-        m = spisok[i][4].split(',')
-        print(m[0] + ',' + ' ваш пароль слишком простой, смените его.')
+count_titul = int(input())
+list_titul = []
+for i in range(count_titul):
+    titul = []
+    name = str(input())
+    while name != '*':
+        titul += name.strip().split()
+        name = str(input())
+    list_titul.append('-'.join(titul))
+    titul = []
+print(', '.join(list_titul[::-1]))
