b37598a3e415a2eb0fb8aac8d941b82ecb92210b
Алексей Горнеев <gorneev1998@mail.ru>
2021-03-02 12:02:30 +0500

    2.7 Знаков без пробелов

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
-inp = input().split(" ")
-res = []
-for a in inp:
-    if a.isdigit() or (a[0] == '-' and a[1:].isdigit()):  # !!!
-        res.append(int(a))
-    elif a == "+":
-        a2 = res.pop()
-        a1 = res.pop()
-        res.append(a1 + a2)
-    elif a == "-":
-        a2 = res.pop()
-        a1 = res.pop()
-        res.append(a1 - a2)
-    elif a == "*":
-        a2 = res.pop()
-        a1 = res.pop()
-        res.append(a1 * a2)
-    elif a == "/":
-        a2 = res.pop()
-        a1 = res.pop()
-        res.append(a1 // a2)
-    elif a == "~":
-        a2 = res.pop()
-        res.append(-a2)
-    elif a == "#":
-        a2 = res.pop()
-        res.append(a2)
-        res.append(a2)
-    elif a == "@":
-        a3 = res.pop()
-        a2 = res.pop()
-        a1 = res.pop()
-        res.append(a2)
-        res.append(a3)
-        res.append(a1)
-    elif a == "!":
-        a1 = res.pop()
-        p = 1
-        for i in range(2, a1 + 1):
-            p *= i
-        res.append(p)
-print(res[0])
+m = input().split()
+a = 0
+for i in range(len(m)):
+    a = a + len(m[i])
+print(a)
