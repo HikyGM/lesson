99cd8465cfc10693b5513f22ebae448fb122938f
Алексей Горнеев <gorneev1998@mail.ru>
2021-03-02 11:49:18 +0500

    2.7 Польский калькулятор

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-count_titul = int(input())
-list_titul = []
-for i in range(count_titul):
-    titul = []
-    name = str(input())
-    while name != '*':
-        titul += name.strip().split()
-        name = str(input())
-    list_titul.append('-'.join(titul))
-    titul = []
-print(', '.join(list_titul[::-1]))
+inp = input().split(" ")
+res = []
+for a in inp:
+    if a.isdigit() or (a[0] == '-' and a[1:].isdigit()):  # !!!
+        res.append(int(a))
+    elif a == "+":
+        a2 = res.pop()
+        a1 = res.pop()
+        res.append(a1 + a2)
+    elif a == "-":
+        a2 = res.pop()
+        a1 = res.pop()
+        res.append(a1 - a2)
+    elif a == "*":
+        a2 = res.pop()
+        a1 = res.pop()
+        res.append(a1 * a2)
+    elif a == "/":
+        a2 = res.pop()
+        a1 = res.pop()
+        res.append(a1 // a2)
+    elif a == "~":
+        a2 = res.pop()
+        res.append(-a2)
+    elif a == "#":
+        a2 = res.pop()
+        res.append(a2)
+        res.append(a2)
+    elif a == "@":
+        a3 = res.pop()
+        a2 = res.pop()
+        a1 = res.pop()
+        res.append(a2)
+        res.append(a3)
+        res.append(a1)
+    elif a == "!":
+        a1 = res.pop()
+        p = 1
+        for i in range(2, a1 + 1):
+            p *= i
+        res.append(p)
+print(res[0])
