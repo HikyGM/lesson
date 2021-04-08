1f2c06cea5f8cf5d878e52236dd327109a7b295e
Алексей Горнеев <gorneev1998@mail.ru>
2021-04-08 11:45:47 +0500

    2.8 CSV

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-n = int(input())
-m = int(input())
 qwe = []
-table = []
-for i in range(n):
-    for j in range(m):
-        qwe.append(input())
-    table.append(qwe)
-    qwe = []
-for row in table:
-    print(*row, sep="\t")
-print()
-for i in range(len(table[i])):
-    for j in range(len(table)):
-        print(table[j][i], sep='\t', end='\t')
-    print()
+for i in range(int(input())):
+    n = input()
+    qwe.append(n.split(','))
+for i in range(int(input())):
+    m = str(input())
+    print(qwe[int(m[0])][int(m[2])])
