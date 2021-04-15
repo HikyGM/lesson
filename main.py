7255adf1d2067388d6a67b074e4235e4c84daff2
HikyGM <hikygm@yandex.ru>
2021-04-15 11:21:12 +0500

    2.9 Толковый словарь

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-n = input().split(' ')
-n.insert(0, '0')
-m = input().split(' ')
-m.insert(0, '0')
-for i in range(1, len(n)):
-    if i == 1:
-        print(m[int(n[i])].capitalize(), end=' ')
+n = int(input())
+spisok = {}
+count = 0
+lis = []
+for i in range(n):
+    slovo = str(input())
+    for j in range(len(slovo)):
+        if slovo[j] == ' ':
+            count = j
+            break
+        else:
+            continue
+    spisok[slovo[:count]] = slovo[count + 1:]
+    count = 0
+m = int(input())
+for i in range(m):
+    search = str(input())
+    lis.append(search)
+for i in range(len(lis)):
+    if lis[i] in spisok:
+        print(spisok[lis[i]])
     else:
-        print(m[int(n[i])].lower(), end=' ')
+        print('Нет в словаре')
